8bd935f87e0ed292cd15a8bb8ac1de978cd4c12b
mascDriver <diogoblibi@hotmail.com>
2021-11-03 14:24:31 -0300

    first commit

---
+++ src/leilao/dominio.py
@@ -0,0 +1,53 @@
+class Usuario:
+
+    def __init__(self, nome, carteira):
+        self.__nome = nome
+        self.__carteira = carteira
+
+    def propoe_lance(self, leilao, valor):
+        if valor > self.__carteira:
+            raise ValueError('Não pode propor um lance com o valor maior que o valor da carteira')
+        lance = Lance(self, valor)
+        leilao.propoe(lance)
+
+        self.__carteira -= valor
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @property
+    def carteira(self):
+        return self.__carteira
+
+
+class Lance:
+
+    def __init__(self, usuario, valor):
+        self.usuario = usuario
+        self.valor = valor
+
+
+class Leilao:
+
+    def __init__(self, descricao):
+        self.descricao = descricao
+        self.maior_lance = 0.0
+        self.menor_lance = 0.0
+        self.__lances = []
+
+    def propoe(self, lance: Lance):
+        if not self.__lances or self.__lances[-1].usuario != lance.usuario and lance.valor > self.__lances[-1].valor:
+            if not self.__lances:
+                self.menor_lance = lance.valor
+
+            self.maior_lance = lance.valor
+
+            self.__lances.append(lance)
+        else:
+            raise ValueError('Erro ao propor lance')
+
+    @property
+    def lances(self):
+        return self.__lances[:]
+
